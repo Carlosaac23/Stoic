a16dd5ec535d60f303d20123d3badd507a77c6d4
Carlos <carlosaac232001@gmail.com>
2025-10-25 22:36:03 -0500

    refactor: change Quote component name to QuoteSection and delete fetch code due quotes are fetching from the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Header from '@/components/Header';
-import Quote from '@/components/Quote';
-import getRandomQuote from '@/lib/quotes';
+import QuoteSection from '@/components/Quote';
 
 export default async function Home() {
-  const quote = await getRandomQuote();
-
   return (
     <main className='mx-10 my-8'>
       <Header />
-      {quote ? <Quote quote={quote} /> : ''}
+      <QuoteSection />
     </main>
   );
 }
