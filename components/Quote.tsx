aea544e880f18c1a8aa3a747d1fb2b3a8a3a0725
Carlos <carlosaac232001@gmail.com>
2025-10-25 22:33:38 -0500

    refactor: - use 'use client' directive - use useEffect to fetch quotes every 20s - use motion to add blur every new quote

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,16 +1,43 @@
-import type { Quote } from '@/lib/quotes';
+'use client';
 
-export default async function Quote({ quote }: { quote: Quote }) {
-  const { author, quote: quoteText } = quote;
+import type { Quote } from '@/lib/quotes';
+import getRandomQuote from '@/lib/quotes';
+import { AnimatePresence, motion } from 'motion/react';
+import { useEffect, useState } from 'react';
+
+export default function QuoteSection() {
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  useEffect(() => {
+    async function fetchQuote() {
+      const newQuote = await getRandomQuote();
+      if (!newQuote) throw new Error('Error fetching quote');
+      setQuote(newQuote);
+    }
+
+    fetchQuote();
+    const interval = setInterval(fetchQuote, 20000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!quote) return <p>Loading quote...</p>;
 
   return (
     <section className='mt-6 rounded-md border border-neutral-300 p-4 shadow-sm'>
-      <blockquote cite='https://www.goodreads.com/quotes/139512-the-happiness-of-your-life-depends-upon-the-quality-of'>
-        <p className='text-sm italic'>&quot; {quoteText} &quot;</p>
-        <p className='text-sm'>
-          - <strong>{author}</strong>
-        </p>
-      </blockquote>
+      <AnimatePresence mode='wait'>
+        <motion.blockquote
+          key={quote.quote}
+          initial={{ opacity: 0, filter: 'blur(10px)' }}
+          animate={{ opacity: 1, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, filter: 'blur(10px)' }}
+          transition={{ duration: 0.6 }}
+        >
+          <p className='text-sm italic'>&quot; {quote.quote} &quot;</p>
+          <p className='text-sm'>
+            - <strong>{quote.author}</strong>
+          </p>
+        </motion.blockquote>
+      </AnimatePresence>
     </section>
   );
 }
