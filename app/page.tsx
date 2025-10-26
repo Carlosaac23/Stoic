a0066ef55a034bce1e2e596defede12b2eb7f320
Carlos <carlosaac232001@gmail.com>
2025-10-25 22:16:04 -0500

    feat: use new Quote component to show a random stoic quote

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
 import Header from '@/components/Header';
+import Quote from '@/components/Quote';
+import getRandomQuote from '@/lib/quotes';
 
-export default function Home() {
+export default async function Home() {
+  const quote = await getRandomQuote();
+
   return (
     <main className='mx-10 my-8'>
       <Header />
-
-      <section className='mt-6 rounded-md border border-neutral-300 p-4 shadow-sm'>
-        <blockquote cite='https://www.goodreads.com/quotes/139512-the-happiness-of-your-life-depends-upon-the-quality-of'>
-          <p className='text-sm italic'>
-            &quot; The happiness of your life depends upon the quality of your
-            thoughts. &quot;
-          </p>
-          <p className='text-sm'>
-            - <strong>Marcus Aurelius</strong>, <cite>Meditations</cite>
-          </p>
-        </blockquote>
-      </section>
+      {quote ? <Quote quote={quote} /> : ''}
     </main>
   );
 }
