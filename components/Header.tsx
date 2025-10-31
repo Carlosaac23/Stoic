8c0a40c45420d09100399b34c9ddfa7054dad2d8
Carlos <carlosaac232001@gmail.com>
2025-10-30 22:17:35 -0500

    feat: made Stoic main name a Link to root page

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className='mx-10 mt-4 flex items-center justify-between px-6 py-4'>
-      <h1 className='text-xl font-bold'>Stoic</h1>
+      <h1 className='text-xl font-bold'>
+        <Link href='/'>Stoic</Link>
+      </h1>
 
       <nav className='flex gap-3 text-sm'>
         <Link
