fc5be891b9ad4010f5f37480fa0a156ed92cad21
Carlos <carlosaac232001@gmail.com>
2025-10-30 22:11:17 -0500

    style: remove margin in y axis

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='flex items-center justify-between rounded-md border border-neutral-300 px-6 py-4 shadow-sm'>
+    <header className='mx-10 mt-4 flex items-center justify-between px-6 py-4'>
       <h1 className='text-xl font-bold'>Stoic</h1>
 
       <nav className='flex gap-3 text-sm'>
