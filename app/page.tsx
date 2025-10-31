395e3c6ff2d6bd955cb9e31cfa7cfb73cd363940
Carlos <carlosaac232001@gmail.com>
2025-10-30 22:12:58 -0500

    feat: create main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import QuoteSection from '@/components/QuoteSection';
 
 export default function Home() {
-  return <QuoteSection />;
+  return (
+    <>
+      <QuoteSection />
+
+      <section className='mt-6'>
+        <h2 className='mb-2 tracking-wide'>What is Stoicism?</h2>
+        <p className='text-sm leading-6'>
+          Stoicism is a Hellenistic philosophy that flourished in ancient Greece
+          and Rome. The Stoics believed that the universe operated according to
+          reason, i.e. by a God which is immersed in nature itself. Of all the
+          schools of ancient philosophy, Stoicism made the greatest claim to
+          being utterly systematic.
+        </p>
+      </section>
+    </>
+  );
 }
